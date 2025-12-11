81fb68581d8e99d7156cbf20789c549696ac9b46
gaudesp <gaudespro@gmail.com>
2025-12-11 15:35:56 +0100

    :truck: Migrate imports to new feature structure and remove deprecated files

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { NotificationsProvider } from "./components/notifications/Notifications";
+import { NotificationsProvider } from "./app/providers/NotificationsProvider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
