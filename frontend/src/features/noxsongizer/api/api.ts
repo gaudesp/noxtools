c30959b80d4842b75a10f0695a564208f50ff6f2
gaudesp <gaudespro@gmail.com>
2025-12-11 14:42:04 +0100

    :truck: Move Noxsongizer feature into isolated feature folder

---
+++ frontend/src/features/noxsongizer/api/api.ts
@@ -0,0 +1,13 @@
+export {
+  uploadNoxsongizer,
+  listNoxsongizerJobs,
+  getNoxsongizerDownloadUrl,
+  getNoxsongizerSourceUrl,
+  type NoxsongizerUploadItem,
+  type NoxsongizerUploadResponse,
+  type NoxsongizerJobResult,
+  type NoxsongizerJob,
+  type Job,
+  type JobTool,
+  type JobStatus,
+} from "../../../lib/api"
